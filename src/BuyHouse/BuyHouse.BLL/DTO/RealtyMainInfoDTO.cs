3bb20054f0d55c5f06e78cc447d96669280666cb
unknown <vlad.ptrv83530@gmail.com>
2022-09-07 23:58:21 +0300

    Flat DTO, repository, service(without realization)
    Add Flat DTO, repository and service without realization

---
+++ src/BuyHouse/BuyHouse.BLL/DTO/RealtyMainInfoDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuyHouse.BLL.DTO
+{
+    public class RealtyMainInfoDTO
+    {
+        public string Region { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string HouseNumber { get; set; }
+        public int FlatNumber { get; set; }
+        public DateTime? RegistrationDate { get; set; }
+    }
+}
---
+++ src/BuyHouse/BuyHouse.BLL/Services/FlatAdvertService.cs
@@ -0,0 +1,30 @@
+﻿using BuyHouse.BLL.DTO.AdvertDTO;
+using BuyHouse.BLL.Services.Abstract;
+using BuyHouse.DAL.Repositories.Abstract;
+
+namespace BuyHouse.BLL.Services
+{
+    public class FlatAdvertService : IFlatAdvertService
+    {
+        private readonly IFlatAdvertRepository _flatAdvertRepository;
+        public FlatAdvertService(IFlatAdvertRepository flatAdvertRepository)
+        {
+            _flatAdvertRepository = flatAdvertRepository;
+        }
+
+        public void AddFlatAdvert(FlatAdvertDTO flatAdvert)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteFlatAdvert(int flatAdvertId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateFlatAdvert(FlatAdvertDTO flatAdvert)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
